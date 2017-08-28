574f50d30b4d6b15a6372a4e4c07df49b30e020c
Daniel Garcia and John Michael <john@>
2017-08-28 11:28:07 -0700

    add frontend logic for clicking and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
-// Test to check if branching is working
+$(document).ready(function() {
+  $("#chester").click(function() {
+    $("#dog1").slideToggle();
+    $("#descChester").slideToggle();
+
+  });
+  $("#brownie").click(function() {
+    $("#dog2").slideToggle();
+    $("#descBrownie").slideToggle();
+
+  });
+  $("#admiral").click(function() {
+    $("#dog3").slideToggle();
+    $("#descAdmiral").slideToggle();
+
+  });
+  $("#leo").click(function() {
+    $("#cat1").slideToggle();
+    $("#descLeo").slideToggle();
+
+  });
+  $("#pharoah").click(function() {
+    $("#cat2").slideToggle();
+    $("#descPharoah").slideToggle();
+
+  });
+  $("#shortie").click(function() {
+    $("#cat3").slideToggle();
+    $("#descShortie").slideToggle();
+
+  });
+});
